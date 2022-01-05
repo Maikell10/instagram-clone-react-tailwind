47840cc8fb1d2e1baf5d6c37e8a46a7dce29f884
Maikell <maikell.ods10@gmail.com>
2022-01-05 00:52:36 -0400

    Login and Sign Up pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import useAuthListener from "./hooks/use-auth-listener";
+import UserContext from "./context/user";
 
+const Dashboard = lazy(() => import("./pages/dashboard"));
 const Login = lazy(() => import("./pages/login"));
+const Signup = lazy(() => import("./pages/signup"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
-function App() {
+export default function App() {
+    const { user } = useAuthListener();
+
     return (
-        <Router>
-            <Suspense fallback={<p>Loading...</p>}>
-                <Routes>
-                    <Route path={ROUTES.LOGIN} element={<Login />} />
-                </Routes>
-            </Suspense>
-        </Router>
+        <UserContext.Provider value={{ user }}>
+            <Router>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Routes>
+                        <Route
+                            path={ROUTES.DASHBOARD}
+                            element={<Dashboard />}
+                        />
+                        <Route path={ROUTES.LOGIN} element={<Login />} />
+                        <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Suspense>
+            </Router>
+        </UserContext.Provider>
     );
 }
-
-export default App;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,34 +2,57 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import * as ROUTES from "../constants/routes";
+import { doesUsernameExist } from "../services/firebase";
 
-export default function Login() {
+export default function SignUp() {
     const navigate = useNavigate();
     const { firebase } = useContext(FirebaseContext);
 
+    const [username, setUsername] = useState("");
+    const [fullName, setFullName] = useState("");
     const [emailAddress, setEmailAddress] = useState("");
     const [password, setPassword] = useState("");
 
     const [error, setError] = useState("");
     const isInvalid = password === "" || emailAddress === "";
 
-    const handleLogin = async (event) => {
+    const handleSignup = async (event) => {
         event.preventDefault();
 
-        try {
-            await firebase
-                .auth()
-                .signInWithEmailAndPassword(emailAddress, password);
-            navigate(ROUTES.DASHBOARD);
-        } catch (err) {
-            setEmailAddress("");
-            setPassword("");
-            setError(err.message);
+        const usernameExists = await doesUsernameExist(username);
+        if (!usernameExists) {
+            try {
+                const createdUserResult = await firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(emailAddress, password);
+
+                await createdUserResult.user.updateProfile({
+                    displayName: username,
+                });
+
+                await firebase.firestore().collection("users").add({
+                    userId: createdUserResult.user.uid,
+                    username: username.toLowerCase(),
+                    fullName,
+                    emailAddress: emailAddress.toLowerCase(),
+                    following: [],
+                    datecreated: Date.now(),
+                });
+
+                navigate(ROUTES.DASHBOARD);
+            } catch (err) {
+                setFullName("");
+                setEmailAddress("");
+                setPassword("");
+                setError(err.message);
+            }
+        } else {
+            setError("That username is already taken, please try another.");
         }
     };
 
     useEffect(() => {
-        document.title = "Login - Instagram";
+        document.title = "Sign Up - Instagram";
     }, []);
 
     return (
@@ -53,7 +76,23 @@ export default function Login() {
                     {error && (
                         <p className="mb-4 text-xs text-red-primary">{error}</p>
                     )}
-                    <form onSubmit={handleLogin} method="POST">
+                    <form onSubmit={handleSignup} method="POST">
+                        <input
+                            type="text"
+                            aria-label="Enter your username"
+                            placeholder="Username"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                            onChange={({ target }) => setUsername(target.value)}
+                            value={username}
+                        />
+                        <input
+                            type="text"
+                            aria-label="Enter your full name"
+                            placeholder="Full Name"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                            onChange={({ target }) => setFullName(target.value)}
+                            value={fullName}
+                        />
                         <input
                             type="text"
                             aria-label="Enter your email address"
@@ -78,18 +117,18 @@ export default function Login() {
                             className={`bg-blue-medium text-white w-full rounded h-8 font-bold"
                             ${isInvalid && "opacity-50"}`}
                         >
-                            Log In
+                            Sign Up
                         </button>
                     </form>
                 </div>
                 <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
                     <p className="text-sm">
-                        Don&apos;t have an account?{" "}
+                        Have an account?{" "}
                         <Link
-                            to={ROUTES.SIGN_UP}
+                            to={ROUTES.LOGIN}
                             className="font-bold text-blue-medium"
                         >
-                            Sign up
+                            Log In
                         </Link>
                     </p>
                 </div>
